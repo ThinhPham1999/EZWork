f87688a5a6c934a1ce3af7db8701c379cf6b8081
Thinh <thinhphu1234567@gmail.com>
2020-08-13 17:13:33 +0700

    Data model(In progress)

--- EZWork.Core/Entities/Career.cs
+++ EZWork.Core/Entities/Career.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EZWork.Core.Entities
 {
-    class Career
+    public class Career
     {
+        [Key]
+        public int CareerId { get; set; }
+        [Required(ErrorMessage = "Name không được để trống")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(1024)]
+        public string Description { get; set; }
+        [StringLength(255)]
+        public string UrlSlug { get; set; }
     }
 }
--- EZWork.Core/Entities/Skill.cs
+++ EZWork.Core/Entities/Skill.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EZWork.Core.Entities
 {
-    class Skill
+    public class Skill
     {
+        [Key]
+        public int SkillId { get; set; }
+        [Required(ErrorMessage = "Name không được để trống")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(1024)]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Trình độ không được để trống")]
+        [Range(0, Int32.MaxValue)]
+        public int Level { get; set; }
+        [StringLength(255)]
+        public string UrlSlug { get; set; }
     }
 }
