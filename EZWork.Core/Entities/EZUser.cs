e3b9b1efaed7f2ab1297ccb3d738737780843f9f
phamquochung23121997 <phamquochung23121997@gmail.com>
2020-08-20 15:51:38 +0700

    Edit_loginAndRegister

--- EZWork.Core/Entities/EZUser.cs
+++ EZWork.Core/Entities/EZUser.cs
@@ -15,8 +15,8 @@ namespace EZWork.Core.Entities
         public string Gender { get; set; }
         public string BirthDay { get; set; }
         public string ImageProfile { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+      //  public string Email { get; set; }
+      // public string PhoneNumber { get; set; }
         public string CreateAt { get; set; }
         public string ModifierAt { get; set; }
         public virtual EZAccount  EZAccount { get; set; }
