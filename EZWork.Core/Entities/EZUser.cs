eefc4b4b4f071a3d78dad612c6242df013f84e55
Thinh <thinhphu1234567@gmail.com>
2020-08-20 16:22:06 +0700

    In progress

--- EZWork.Core/Entities/EZUser.cs
+++ EZWork.Core/Entities/EZUser.cs
@@ -15,10 +15,11 @@ namespace EZWork.Core.Entities
         public string Gender { get; set; }
         public string BirthDay { get; set; }
         public string ImageProfile { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        //public string Email { get; set; }
+        //public string PhoneNumber { get; set; }
         public string CreateAt { get; set; }
         public string ModifierAt { get; set; }
+        public int Status { get; set; }
         public virtual EZAccount  EZAccount { get; set; }
         public virtual Seller Seller { get; set; }
         public virtual IList<Comment> Comments { get; set; }
--- EZWork.WebUI/Controllers/CareerController.cs
+++ EZWork.WebUI/Controllers/CareerController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EZWork.Core.Abstract;
+using EZWork.Core.Repository;
+using EZWork.WebUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +11,24 @@ namespace EZWork.WebUI.Controllers
 {
     public class CareerController : Controller
     {
+        private ICareerRepository careerRepository;
+
+        public CareerController()
+        {
+            careerRepository = new CareerRepository();
+        }
+
         // GET: Career
         public ActionResult Index()
+        {
+            var career = new CareerViewModel()
+            {
+                Careers = careerRepository.getAllCareers()
+            };
+            return View(career);
+        }
+
+        public ActionResult ViewAll()
         {
             return View();
         }
