0f14f70fc7edbe86702a5d71a57d31b691b1011f
trilam18061999 <trilam18061999@gmail.com>
2020-08-25 09:20:08 +0700

    ss

---
+++ EZWork.WebUI/Controllers/UserLogController.cs
@@ -0,0 +1,19 @@
+﻿using EZWork.Core.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EZWork.WebUI.Controllers
+{
+    public class UserLogController : Controller
+    {
+        private EZWorkDbContext db = new EZWorkDbContext();
+        // GET: UserLog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
