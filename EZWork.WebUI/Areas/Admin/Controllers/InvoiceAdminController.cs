28eb5bc06a2a664bc2957dfae9d8349afb031c22
phamquochung23121997 <phamquochung23121997@gmail.com>
2020-09-03 14:22:07 +0700

    nháp k có gì

--- EZWork.WebUI/Areas/Admin/Controllers/InvoiceAdminController.cs
+++ EZWork.WebUI/Areas/Admin/Controllers/InvoiceAdminController.cs
@@ -19,7 +19,7 @@ namespace EZWork.WebUI.Areas.Admin.Controllers
         // GET: Admin/InvoiceAdmin
         public ActionResult Index(string searchTerm, int? page, int? statusCode, string datetimePicker)
         {
-            int recordSize = 3;
+            int recordSize = 1;
             page = page ?? 1;
             statusCode = statusCode ?? 0;
             DateTime? time = Convert.ToDateTime(datetimePicker);
