21d41ef36fc3af9419fdec6694acbbf10b2eaaf1
Thinh <thinhphu1234567@gmail.com>
2020-09-07 16:32:14 +0700

    fix view admin order

--- EZWork.Core/Abstract/IInvoiceRepository.cs
+++ EZWork.Core/Abstract/IInvoiceRepository.cs
@@ -10,8 +10,12 @@ namespace EZWork.Core.Abstract
     public interface IInvoiceRepository
     {
         IEnumerable<Order> GetAllInvoice();
-        IEnumerable<Order> SearchInvoice(string searchTerm, int page, int recordSize,int? statusCode,DateTime? time);
-        int SearchInvoiceCount(string searchTerm, int? statusCode,DateTime? time);
+        IEnumerable<Order> SearchInvoiceNotPay(string searchTerm, int page, int recordSize,int? statusCode,DateTime? time);
+        int SearchInvoiceCountNotPay(string searchTerm, int? statusCode,DateTime? time);
+        IEnumerable<Order> SearchInvoicePaied(string searchTerm, int page, int recordSize, int? statusCode, DateTime? time);
+        int SearchInvoiceCountPaied(string searchTerm, int? statusCode, DateTime? time);
+        IEnumerable<Order> SearchInvoiceCancel(string searchTerm, int page, int recordSize, int? statusCode, DateTime? time);
+        int SearchInvoiceCountCancel(string searchTerm, int? statusCode, DateTime? time);
         Order Detail(int ID);
     }
 }
--- EZWork.WebUI/Areas/Admin/Controllers/InvoiceAdminController.cs
+++ EZWork.WebUI/Areas/Admin/Controllers/InvoiceAdminController.cs
@@ -21,13 +21,27 @@ namespace EZWork.WebUI.Areas.Admin.Controllers
         {
             int recordSize = 1;
             page = page ?? 1;
-            statusCode = statusCode ?? 0;
+            statusCode = statusCode ?? 1;
             DateTime? time = Convert.ToDateTime(datetimePicker);
             ListInvoiceViewModel model = new ListInvoiceViewModel();
             model.DateTimePicker = time.Value;
             model.SearchTerm = searchTerm;
-            model.Orders = invoiceRepository.SearchInvoice(searchTerm, page.Value, recordSize, statusCode, time).ToList();
-            var totalRecord = invoiceRepository.SearchInvoiceCount(searchTerm, statusCode, time);
+            int totalRecord = 0;
+            if (statusCode == 1)
+            {
+                model.Orders = invoiceRepository.SearchInvoicePaied(searchTerm, page.Value, recordSize, statusCode, time).ToList();
+                totalRecord = invoiceRepository.SearchInvoiceCountPaied(searchTerm, statusCode, time);
+            }
+            if (statusCode == 0)
+            {
+                model.Orders = invoiceRepository.SearchInvoiceNotPay(searchTerm, page.Value, recordSize, statusCode, time).ToList();
+                totalRecord = invoiceRepository.SearchInvoiceCountNotPay(searchTerm, statusCode, time);
+            }
+            if (statusCode == 2)
+            {
+                model.Orders = invoiceRepository.SearchInvoiceCancel(searchTerm, page.Value, recordSize, statusCode, time).ToList();
+                totalRecord = invoiceRepository.SearchInvoiceCountCancel(searchTerm, statusCode, time);
+            }
             model.Pager = new Pager(totalRecord, page, recordSize);
             return View(model);
             // return View();
@@ -41,8 +55,22 @@ namespace EZWork.WebUI.Areas.Admin.Controllers
             ListInvoiceViewModel model = new ListInvoiceViewModel();
             model.DateTimePicker = time.Value;
             model.SearchTerm = searchTerm;
-            model.Orders = invoiceRepository.SearchInvoice(searchTerm, page.Value, recordSize, statusCode, time).ToList();
-            var totalRecord = invoiceRepository.SearchInvoiceCount(searchTerm, statusCode, time);
+            int totalRecord = 0;
+            if (statusCode == 1)
+            {
+                model.Orders = invoiceRepository.SearchInvoicePaied(searchTerm, page.Value, recordSize, statusCode, time).ToList();
+                totalRecord = invoiceRepository.SearchInvoiceCountPaied(searchTerm, statusCode, time);
+            }
+            if (statusCode == 0)
+            {
+                model.Orders = invoiceRepository.SearchInvoiceNotPay(searchTerm, page.Value, recordSize, statusCode, time).ToList();
+                totalRecord = invoiceRepository.SearchInvoiceCountNotPay(searchTerm, statusCode, time);
+            }
+            if (statusCode == 2)
+            {
+                model.Orders = invoiceRepository.SearchInvoiceCancel(searchTerm, page.Value, recordSize, statusCode, time).ToList();
+                totalRecord = invoiceRepository.SearchInvoiceCountCancel(searchTerm, statusCode, time);
+            }
             model.Pager = new Pager(totalRecord, page, recordSize);
             return PartialView(model);
             // return View();
